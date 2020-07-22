7a87fd6fc0a97a4f8cf65fff6b31b8f2c1c82a57
charanM1232 <mjcn0624@gmail.com>
2020-07-21 19:54:14 -0700

    added images to slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -6,8 +6,15 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
+
+
         World.add(world, this.sling);
     }
 
@@ -16,11 +23,35 @@ class SlingShot{
     }
 
     display(){
+
+        image(this.sling1,200,100,25,125);
+        
+        
+        image(this.sling2,185,100,25,70);
+ 
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+            push();
+            strokeWeight(8);
+            stroke(48,22,8);
+            if(pointA.x< 220){
+                image(this.sling3,this.sling.bodyA.position.x-20,this.sling.bodyA.position.y,10,20)
+                
+                line(pointA.x-20, pointA.y+5, pointB.x+25, pointB.y+20);
+
+                line(pointA.x-20, pointA.y+5, pointB.x-15, pointB.y+20);
+                
+            } else {
+                image(this.sling3,this.sling.bodyA.position.x+20,this.sling.bodyA.position.y,10,20)
+                
+                line(pointA.x+20, pointA.y+5, pointB.x+25, pointB.y+20);
+
+                line(pointA.x+20, pointA.y+5, pointB.x-15, pointB.y+20); 
+            }
+
         }
     }
     
